353bffdf056316257b9f5361c1d412bd1374055c
Ryan Pilgrim <rzpilgrim@gmail.com>
2022-12-06 23:47:02 -0500

    add data loader for point clouds

--- rotation_estimation/data.py
+++ rotation_estimation/data.py
@@ -1,15 +1,27 @@
 from pathlib import Path
+from typing import Dict, List
 
+import torch
 from pytorch3d.datasets.shapenet.shapenet_core import ShapeNetCore
-from torch.utils.data import IterableDataset
+from pytorch3d.structures import Meshes
+from pytorch3d.ops import sample_points_from_meshes
+from torch.utils.data import DataLoader
+
+from .constants import DEFAULT_BATCH_SIZE, DEFAULT_POINTS_PER_SAMPLE
 
 
-class PointCloudDataset(IterableDataset):
-    def __init__(self, path: Path):
-        super().__init__()
-        self.path = path
-        self.shapenet_core = ShapeNetCore(self.path, version=2)
+class PointCloudCollator:
+    def __init__(self, points_per_sample: int = DEFAULT_POINTS_PER_SAMPLE) -> None:
+        self.points_per_sample = points_per_sample
 
-    def __iter__(self):
-        # iterate over directory structure
-        pass
+    def __call__(self, batch: List[Dict]) -> torch.Tensor:
+        meshes = Meshes(verts=[x["verts"] for x in batch], faces=[x["faces"] for x in batch])
+        return sample_points_from_meshes(meshes, num_samples=self.points_per_sample)  # type: ignore
+
+
+def get_point_cloud_data_loader(
+    path: Path, batch_size: int = DEFAULT_BATCH_SIZE, points_per_sample: int = DEFAULT_POINTS_PER_SAMPLE
+) -> DataLoader:
+    return DataLoader(
+        ShapeNetCore(path, version=2), batch_size=batch_size, collate_fn=PointCloudCollator(points_per_sample)
+    )
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,2 +1,14 @@
-def test_dataset():
-    pass
+from pathlib import Path
+
+import pytest
+
+from rotation_estimation.data import get_point_cloud_data_loader
+
+
+@pytest.mark.parametrize("batch_size,points_per_sample", [(5, 100), (10, 1000)])
+def test_point_cloud_dataloader(batch_size: int, points_per_sample: int):
+    pc_dataloader = get_point_cloud_data_loader(
+        Path(__file__).parents[1] / "data/ShapeNetCore.v2/", batch_size=batch_size, points_per_sample=points_per_sample
+    )
+    batch = next(iter(pc_dataloader))
+    assert batch.size() == (batch_size, points_per_sample, 3)
