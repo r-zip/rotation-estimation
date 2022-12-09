99c51f35889abc94ed28de1d645b1f2e8d8b1e74
Adam Wang <adamw5954@gmail.com>
2022-12-08 21:47:29 -0500

    six d head

---
+++ rotation_estimation/sixd.py
@@ -0,0 +1,11 @@
+import torch
+from torch.nn.modules.loss import _Loss
+import pytorch3d.transforms.rotation_conversions ## 6D AUX functions
+
+class OrthogonalMSELoss(_Loss):
+    def __init__(self, weight: float = 0.01) -> None:
+        super(OrthogonalMSELoss, self).__init__()
+        self.weight = weight
+
+    def forward(self, x, y) -> torch.Tensor:
+        return torch.mean(torch.sum((x-y[:,:2,:])**2, dim=(1,2)) + self.weight * torch.flatten(torch.matmul(x[:,0,:][:,None,:], x[:,1,:][:,:,None])))
