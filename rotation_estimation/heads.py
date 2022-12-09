4fbc8be886e6dad211505100d603dfc66ce276c8
Ryan Pilgrim <rzpilgrim@gmail.com>
2022-12-08 21:50:06 -0500

    update

--- rotation_estimation/heads.py
+++ rotation_estimation/heads.py
@@ -6,25 +6,26 @@ import torch.nn as nn
 from .blocks import build_mlp
 
 
-def svd_projection(R: torch.Tensor) -> torch.Tensor:
+def svd_projection(R: torch.Tensor, eps: float = 0.001) -> torch.Tensor:
     """
     Project R onto SO(3) using the SVD.
 
     Reference: https://proceedings.neurips.cc/paper/2020/file/fec3392b0dc073244d38eba1feb8e6b7-Paper.pdf
     """
     # TODO: test
-    U, _, Vh = torch.linalg.svd(R)
-    det = torch.linalg.det(torch.matmul(U, Vh))
-    if len(R.shape) > 2:
-        S_prime = torch.stack([torch.eye(3) for _ in range(R.shape[0])])
-        S_prime[:, 2, 2] = det
-    elif len(R.shape) == 2:
-        S_prime = torch.eye(3)
-        S_prime[2, 2] = det
-    else:
-        raise ValueError(f"Input tensor R (shape={R.shape}) has too few  dimensions.")
+    U, _, Vh = torch.linalg.svd(R + torch.stack([eps * torch.eye(3) for _ in range(R.shape[0])]))
+    # det = torch.linalg.det(torch.matmul(U, Vh))
+    # if len(R.shape) > 2:
+    #     S_prime = torch.stack([torch.eye(3) for _ in range(R.shape[0])])
+    #     S_prime[:, 2, 2] = det
+    # elif len(R.shape) == 2:
+    #     S_prime = torch.eye(3)
+    #     S_prime[2, 2] = det
+    # else:
+    #     raise ValueError(f"Input tensor R (shape={R.shape}) has too few  dimensions.")
 
-    return torch.matmul(torch.matmul(U, S_prime), Vh)
+    # return torch.matmul(torch.matmul(U, S_prime), Vh)
+    return torch.matmul(U, Vh)
 
 
 class SVDHead(nn.Module):
